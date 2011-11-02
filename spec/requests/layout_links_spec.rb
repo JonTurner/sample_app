d41d3587b8ec14cf2ad088ebca0fa233a7ac3835
Jon Turner <jonmdr@gmail.com>
2011-11-01 22:03:41 -0400

    Added app_helpter for logo.  test layout links in root

--- spec/requests/layout_links_spec.rb
+++ spec/requests/layout_links_spec.rb
@@ -27,6 +27,19 @@ describe "LayoutLinks" do
     response.should have_selector('title', :content => "Sign Up")
   end
 
+  it "should have the right links on the layout" do
+    visit root_path
+    click_link "About"
+    response.should have_selector('title', :content=> "About")
+    click_link "Help"
+    response.should have_selector('title', :content=> "Help")
+    click_link "Contact"
+    response.should have_selector('title', :content=> "Contact")
+    click_link "Home"
+    response.should have_selector('title', :content=> "Home")
+    click_link "Sign up now!"
+    response.should have_selector('title', :content=> "Sign Up")
+  end
   #describe "GET /layout_links" do
   #  it "works! (now write some real specs)" do
   #    # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
